243cd20b98825d9aa3ee9e2d7a12a62964c875f0
alondramora <mora_ac@yahoo.com>
2022-08-24 21:20:00 -0700

    created an async function called run that will create a new user object and console log it

--- Desktop/coding-projects/learningMongoose/User.js
+++ Desktop/coding-projects/learningMongoose/User.js
@@ -7,5 +7,6 @@ const userSchema = new mongoose.Schema({
     age: Number,
 })
 
-// Model name will be the name of a collection in MongoDB
+//module.exports will allow us to use the model in script.js
+// Model name will be the first param and name of a collection in MongoDB
 module.exports = mongoose.model("User", userSchema) // Schema name is passed in
--- Desktop/coding-projects/learningMongoose/script.js
+++ Desktop/coding-projects/learningMongoose/script.js
@@ -1,6 +1,22 @@
 const mongoose = require('mongoose')
 
-//importing the model from User.js
+//importing the model from User.js, we can access this because we used module.exports
 const User = require("./User")
 
 mongoose.connect("mongodb://localhost/testdb")
+
+run()
+async function run() {
+    const user = await User.create({ name: "Kyle", age: 26}) // does the same thing as creating a new user and saving commented code below
+    user.name = "Sally" // renaming the name key to "Sally", when we console log the name will be Sally
+    // const user = new User({ name: "Kyle", age: 26 })
+    // await user.save()
+    console.log(user)
+}
+
+
+//instantiating a new user object to create a new user
+// name and age are keys
+// put them inside of a variable called user
+// const user = new User({ name: "Kyle", age: 26 })
+// user.save().then(() => console.log("User saved"))
